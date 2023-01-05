a35dbdeee13d472dc2e2ba6c3a997b2b4b5625ac
Priya Nair <prisni@gmail.com>
2023-01-05 10:40:53 +0530

    Initial commit

---
+++ src/Q1.java
@@ -0,0 +1,9 @@
+
+public class Q1 {
+    public static void PrintBackwards(int[] numbers){
+        for (int i=numbers.length-1; i>0; i--){
+            System.out.println(numbers[i]);
+        }
+        
+    }
+}
